3a8040c661897e5b405d7d8df582074da2271ff9
Sabrina Shawon <129847266+sabrina991@users.noreply.github.com>
2024-11-24 21:25:53 +0600

    Completing Home page design

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,13 +5,11 @@ import "./Home.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-
 const Home = () => {
-    const [togglebar, setTogglebar] = useState(false);
-    const ShowHeader = () => {
-        setTogglebar(!togglebar);
+  const [togglebar, setTogglebar] = useState(false);
+  const ShowHeader = () => {
+    setTogglebar(!togglebar);
   };
-
   gsap.registerPlugin(useGSAP);
   useGSAP(() => {
     gsap.from(".h-nav-div", {
@@ -29,10 +27,10 @@ const Home = () => {
       stagger: 0.1,
     });
   }, {});
-    
-return (
+
+  return (
     <div>
-    <header className="header">
+      <header className="header">
         <nav className="h-nav">
           <div className="h-nav-div">
             <h2 className="h-nav-div-h2">LabAssist</h2>
@@ -85,7 +83,7 @@ return (
         </nav>
       </header>
 
-    <section className="wrapper">
+      <section className="wrapper">
         <div className="w-div container">
           <div className="w-div-div grid-cols-2">
             {/* Text item to the left */}
@@ -125,7 +123,102 @@ return (
           </div>
         </div>
       </section>
-    
+
+      <section className="wrapper" id="features">
+        <div className="container">
+          <div className="grid-cols-4">
+            <div className="grid-cols-item">
+              <div className="grid-icon">
+                <i
+                  className="fa fa-address-card"
+                  style={{ fontSize: "48px", color: "rgb(0, 75, 161)" }}
+                ></i>
+              </div>
+              <div className="featured-info">
+                <span>User Management System</span>
+                <p>
+                  One of the major features is to introduce user authentication
+                  and login for the students, teachers, and lab instructors. It
+                  will help to automate the access system in the laboratory.
+                </p>
+              </div>
+            </div>
+
+            <div className="grid-cols-item">
+              <div className="grid-icon">
+                <i
+                  className="fa fa-cog fa-spin"
+                  style={{ fontSize: "48px", color: "rgb(0, 75, 161)" }}
+                ></i>
+              </div>
+              <div className="featured-info">
+                <span>Online Request & Approval</span>
+                <p>
+                  The main feature of our project is a student can request for
+                  the equipment without being physically present. Teachers and
+                  admins can view and approve the request.
+                </p>
+              </div>
+            </div>
+            <div className="grid-cols-item">
+              <div className="grid-icon">
+                <i
+                  className="fa fa-clipboard"
+                  style={{ fontSize: "48px", color: "rgb(0, 75, 161)" }}
+                ></i>
+              </div>
+              <div className="featured-info">
+                <span>Fully Digitalized System</span>
+                <p>
+                  This web application can help to reduce the use of
+                  hand-written paper documents. It introduces digital documents
+                  that keep the records of the instrument's usage by the
+                  students.
+                </p>
+              </div>
+            </div>
+            <div className="grid-cols-item">
+              <div className="grid-icon">
+                <i
+                  className="fa fa-address-card"
+                  style={{ fontSize: "48px", color: "rgb(0, 75, 161)" }}
+                ></i>
+              </div>
+              <div className="featured-info">
+                <span>Interactive Web Service</span>
+                <p>
+                  MERN stack based application meets the user experience in a
+                  smarter, more efficient manner. Enjoy lab environment with our
+                  LabAssist developed to meet the unique needs of laboratory
+                  management.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer className="footer" id="contact">
+        <div className="footer-content">
+          <h3>LabAssist: Lab Management System</h3>
+          <p>
+            This site is developed for the use of education purpose. With the
+            hard effort of 3 students from 3rd batch of IRE Department of BDU,
+            the site is ready to serve its purpose.{" "}
+          </p>
+        </div>
+        <div className="contact">
+          <p>Phone no: 01641578822</p>
+          <p>Email: 2001011@bdu.ac.bd</p>
+        </div>
+        <div className="footer-bottom">
+          <p>
+            copyright &copy;2023 <a href="#">LabAssist</a>{" "}
+          </p>
+        </div>
+      </footer>
     </div>
   );
-}
+};
+
+export default Home;
